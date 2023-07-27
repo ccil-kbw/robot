8701c03755fe47f1224b3f6c21efa7c08aa35f1b
serafdev <fares@seraf.dev>
2023-07-27 11:26:07 -0400

    feat: added periodical config refresh

--- cmd/darsrec/main.go
+++ cmd/darsrec/main.go
@@ -3,10 +3,30 @@ package main
 import (
 	"fmt"
 	"os"
+	"sync"
 	"time"
 
 	"github.com/ccil-kbw/robot/rec"
 )
+
+// AsyncConf (sync.Mutex) is used to handle the RecordConfig asynchronously as it's used in the main and some sub routines
+type AsyncConf struct {
+	mu    sync.Mutex
+	confs []*rec.RecordConfig
+}
+
+// Refresh updates the scheduling configurations
+func (ac *AsyncConf) Refresh() {
+	ac.mu.Lock()
+	ac.confs = rec.GetIqamaRecordingConfigs()
+	ac.mu.Unlock()
+}
+
+func (ac *AsyncConf) Confs() []*rec.RecordConfig {
+	ac.mu.Lock()
+	defer ac.mu.Unlock()
+	return ac.confs
+}
 
 func main() {
 
@@ -23,15 +43,25 @@ func main() {
 
 	defer client.Disconnect()
 
-	confs := rec.GetIqamaRecordingConfigs()
+	ac := AsyncConf{}
+	ac.Refresh()
+
+	ticker := time.NewTicker(1 * time.Hour)
+	go func() {
+		for range ticker.C {
+			fmt.Printf("%s: Updating Iqama time...\n", time.Now().Format(time.Kitchen))
+			ac.Refresh()
+		}
+	}()
 
 	for {
+
 		isRecording, err := client.IsRecording()
 		if err != nil {
 			panic(err)
 		}
 
-		shouldRecord := rec.SupposedToBeRecording(confs)
+		shouldRecord := rec.SupposedToBeRecording(ac.Confs())
 
 		// Start recording if supposed to be recording but currently not recording
 		if shouldRecord && !isRecording {
